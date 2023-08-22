16bd1edd8f41057cdda1d5dae71a4751d97e44c0
hasinihewa <136579415+hasinihewa@users.noreply.github.com>
2023-08-22 07:21:09 +0530

    Add files via upload

---
+++ question3.scala
@@ -0,0 +1,61 @@
+package labsheet9
+
+object question3 {
+  class Account(initialBalance: Double) {
+    private var balance: Double = initialBalance
+
+    def getBalance: Double = balance
+
+    def deposit(amount: Double): Unit = {
+      if (amount > 0) {
+        balance += amount
+        println(s"Deposited $amount. New balance: $balance")
+      } else {
+        println("Deposit amount must be positive.")
+      }
+    }
+
+    def withdraw(amount: Double): Unit = {
+      if (amount > 0 && amount <= balance) {
+        balance -= amount
+        println(s"Withdrew $amount. New balance: $balance")
+      } else {
+        println("Withdrawal amount must be positive and cannot exceed the balance.")
+      }
+    }
+
+    def transfer(amount: Double, toAccount: Account): Unit = {
+      if (amount > 0 && amount <= balance) {
+        balance -= amount
+        toAccount.deposit(amount)
+        println(s"Transferred $amount to ${toAccount.toString}. New balance: $balance")
+      } else {
+        println("Transfer amount must be positive and cannot exceed the balance.")
+      }
+    }
+
+    override def toString: String = s"Account balance: $balance"
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val account1 = new Account(2000.0)
+    val account2 = new Account(1000.0)
+
+    println(account1)
+    println(account2)
+
+    account1.deposit(700.0)
+    account2.withdraw(800.0)
+
+    account1.transfer(300.0, account2)
+
+    println(account1)
+    println(account2)
+  }
+
+
+}
+
+
+
